c429e5bfbade9b8acae7e29f303939dd02c719ee
lukasRevol <lukasz.revol@gmail.com>
2023-12-02 21:48:06 +0100

    finished

--- js/main.js
+++ js/main.js
@@ -1,9 +1,35 @@
-const ball = document.querySelector('.ball')
-const input = document.querySelector('input')
-const answer = document.querySelector('.answer')
+const ball = document.querySelector('.ball');
+const input = document.querySelector('input');
+const answer = document.querySelector('.generated-answer');
+const eroor = document.querySelector('.error');
+
+const answersArray = [
+	'raczej nie',
+	'nie chcesz znać odpowiedzi',
+	'zapytaj starej',
+	'Zdecydowanie tak',
+	'50/50',
+	'Jak by sie nie dało to pan bóg by inaczej świat stworzył',
+	'może',
+	'lepiej nie',
+	'nadzieja umiera ostatnia',
+	'szansa niewielka, ale zawsze jest',
+];
 
 ball.addEventListener('click', () => {
-    if (input.value !== '' && input.value.at(-1) === '?'){
-    answer.textContent = 'dfsa'}
+	ball.classList.remove('ball-shake');
+	if (input.value === '') {
+		eroor.textContent = 'podaj pytanie';
+	} else if (input.value.at(-1) !== '?') {
+		eroor.textContent = 'pytanie musi być zakończone znakiem zapytania';
+	} else {
+		ball.classList.add('ball-shake');
+		eroor.textContent = '';
+		setTimeout(() => {
+			x = Math.floor(Math.random() * answersArray.length);
+			answer.textContent = answersArray[x];
+			ball.classList.remove('ball-shake');
+		}, 2000);
+	}
+	input.value = '';
 });
-// console.log(input.value.at(-1));
